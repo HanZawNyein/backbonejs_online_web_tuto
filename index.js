19c7e58eaf64a7119b228ea2a7052e39fbb120f1
HanZawNyein <hanzawnyineonline@gmail.com>
2023-01-26 15:09:06 +0630

    backbone view

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     var FirstView = Backbone.View.extend({
+        el: "#content",
+        template: _.template($("#eventTemplate").html()),
+        events:{
+            "click #btnClick":"onClick"
+        },
         initialize: function () {
             this.render();
         },
         render: function () {
-            console.log("Hello OWT");
+            this.$el.html(this.template());
+            return this;
         },
+        onClick:function(){
+            alert(
+                "Hey Event"
+            )
+        }
     });
 
     var firstview = new FirstView();
